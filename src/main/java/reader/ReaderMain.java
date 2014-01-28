be3f881763600289e56c31340c2257b9ed8e645e
Marcus Simonsen <msimonsen@blackberry.com>
2014-01-28 08:56:21 -0500

    Adding changes that will be refactored or pulled out.

--- src/main/java/reader/ReaderMain.java
+++ src/main/java/reader/ReaderMain.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.datastax.driver.core.Cluster;
 
 import driver.em.CUtils;
@@ -13,16 +16,19 @@ import driver.em.CassConfig;
 public class ReaderMain {
 
 	static Cluster cluster = null;
-	
+	static Logger logger = LoggerFactory.getLogger(ReaderMain.class);
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
 		
 		String configFile = System.getProperty("config");
+		String startToken = System.getProperty("startToken");
+		String endToken = System.getProperty("endToken");
+		
 		if (configFile == null)
 			configFile = "reader-config.xml";
-				
+		
 		CQLRowReader reader = new CQLRowReader();
 		
 		JAXBContext jc = JAXBContext.newInstance(ReaderConfig.class);
@@ -30,15 +36,29 @@ public class ReaderMain {
 		InputStream ins = Thread.currentThread().getContextClassLoader().getResourceAsStream(configFile);
 		
 		reader.config = (ReaderConfig)unmarshaller.unmarshal(ins);
-		
+		if (startToken!=null)
+			reader.config.setStartToken(Long.parseLong(startToken));
+		if (endToken!=null)
+			reader.config.setEndToken(Long.parseLong(endToken));
 		reader.cluster = CUtils.createCluster(reader.config.getCassConfig());
 		reader.session = reader.cluster.connect(reader.config.getKeyspace());
 		
 		//for exception safety below
 		Class.forName( reader.config.getReaderTask() ).newInstance();
+		try {
+			reader.read();
+		}catch (Exception e) {
+			logger.error(e.getMessage(),e);
+		}
 		
-		reader.read();
-		
+		//hack util we have proper reporting and job handling
+		RowReaderTask<?> rowReader = (RowReaderTask<?>)Class.forName( reader.config.getReaderTask() ).newInstance();
+		if (rowReader instanceof LargeRowsTask) {
+			LargeRowsTask task = (LargeRowsTask)rowReader;
+			task.printAll();
+		}
+		//end hack
+		logger.info("Shutting down cluster");
 		reader.cluster.shutdown();
 		
 	}
